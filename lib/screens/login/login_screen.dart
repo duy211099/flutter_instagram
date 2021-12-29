b4bf6df5045f15bd6ea73b851f21fc6b340a184a
Duy <1710156@dlu.edu.vn>
2021-12-29 21:41:38 +0700

    connect login_cubit

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,91 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_instagram/repositories/auth/auth_repository.dart';
+import 'package:flutter_instagram/screens/login/cubit/login_cubit.dart';
 
 class LoginScreen extends StatelessWidget {
   static const String routeName = '/login';
+
+  LoginScreen({Key? key}) : super(key: key);
+
   static Route route() {
     return PageRouteBuilder(
-        settings: const RouteSettings(name: routeName),
-        transitionDuration: const Duration(seconds: 0),
-        pageBuilder: (_, __, ___) => LoginScreen());
+      settings: const RouteSettings(name: routeName),
+      transitionDuration: const Duration(seconds: 0),
+      pageBuilder: (context, _, __) => BlocProvider(
+        create: (_) =>
+            LoginCubit(authRepository: context.read<AuthRepository>()),
+        child: LoginScreen(),
+      ),
+    );
   }
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<LoginCubit>();
     return WillPopScope(
       onWillPop: () async => false,
       child: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Card(
+        child: BlocConsumer<LoginCubit, LoginState>(
+          listener: (context, state) {
+            if (state.status == LoginStatus.error) {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('Error'),
+                  content: Text(state.failure.message),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: Center(
                 child: Padding(
                   padding: const EdgeInsets.all(24.0),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Instagram',
-                          style: TextStyle(
-                              fontSize: 28, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Instagram',
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 12),
+                            TextFormField(
+                              decoration:
+                                  const InputDecoration(hintText: 'Email'),
+                              onChanged: (value) => cubit.emailChanged(value),
+                              validator: (value) => !value!.contains('@')
+                                  ? 'Please enter a valid email.'
+                                  : null,
+                            ),
+                            const SizedBox(height: 12),
+                            TextFormField(
+                              obscureText: true,
+                              decoration:
+                                  const InputDecoration(hintText: 'Password'),
+                              onChanged: (value) =>
+                                  cubit.passwordChanged(value),
+                              validator: (value) => value!.length < 6
+                                  ? 'Must be at least 6 characters.'
+                                  : null,
+                            ),
+                            const SizedBox(height: 28),
+                            ElevatedButton(
+                              onPressed: () => _submitForm(
+                                context,
+                                state.status == LoginStatus.submitting,
+                              ),
+                              child: const Text('Login'),
+                              style: ElevatedButton.styleFrom(
+                                elevation: 1,
+                              ),
+                            ),
+                            const SizedBox(height: 12),
+                            ElevatedButton(
+                              onPressed: () {
+                                print('signup');
+                              },
+                              child: const Text('No account? Sign up'),
+                              style: ElevatedButton.styleFrom(
+                                elevation: 1,
+                                primary: Colors.grey[200],
+                                onPrimary: Colors.black,
+                              ),
+                            )
+                          ],
                         ),
-                        const SizedBox(height: 12),
-                        TextFormField(
-                          decoration: const InputDecoration(hintText: 'Email'),
-                          onChanged: (value) => print(value),
-                          validator: (value) => !value!.contains('@')
-                              ? 'Please enter a valid email.'
-                              : null,
-                        ),
-                        const SizedBox(height: 12),
-                        TextFormField(
-                          decoration:
-                              const InputDecoration(hintText: 'Password'),
-                          onChanged: (value) => print(value),
-                          validator: (value) => value!.length < 6
-                              ? 'Must be at least 6 characters.'
-                              : null,
-                        ),
-                        const SizedBox(height: 28),
-                        ElevatedButton(
-                          onPressed: () {
-                            print('Login');
-                          },
-                          child: Text('Login'),
-                          style: ElevatedButton.styleFrom(
-                            elevation: 1,
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-                        ElevatedButton(
-                          onPressed: () {
-                            print('Navigate to signup');
-                          },
-                          child: Text('No account? Sign up'),
-                          style: ElevatedButton.styleFrom(
-                            elevation: 1,
-                            primary: Colors.grey[200],
-                            onPrimary: Colors.black,
-                          ),
-                        )
-                      ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
+  }
+
+  void _submitForm(BuildContext context, bool isSubmitting) {
+    if (_formKey.currentState!.validate() && !isSubmitting) {
+      context.read<LoginCubit>().logInWithCredential();
+    }
   }
 }
